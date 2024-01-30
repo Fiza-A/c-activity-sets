447e0c9b91952167a3fcb3693d1f34885a4ce08d
Fiza-A <fizaanwar.bsc23@rvu.edu.in>
2024-01-30 11:02:10 +0530

    test5

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,39 @@
+/*5. Write a C program to compare three numbers using **pass by value**
 
+***Function Declarations***
+
+```c
+int input();
+int compare(int a, int b, int c);
+void output(int a, int b, int c, int largest);
+```*/
+#include<stdio.h>
+int input(){
+    int a;
+    printf("Enter a number:");
+    scanf("%d",&a);
+    return a;
+}
+int compare(int a, int b, int c){
+    int large=a;
+    if(b>a){
+        large=b;
+    }
+    if(c>a){
+        large=c;
+    }
+    return large;
+}
+void output(int a, int b, int c, int large){
+    printf("largest number is %d",large);
+}
+int main(){
+    int a, b, c;
+    a=input();
+    b=input();
+    c=input();
+    int large;
+    large=compare(a, b, c);
+    output(a,b,c,large);
+    return 0;
+}
