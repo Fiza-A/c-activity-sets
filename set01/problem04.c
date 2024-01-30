e50b06427e2e3d868736baa402aeb62fe170fa51
Fiza-A <fizaanwar.bsc23@rvu.edu.in>
2024-01-30 10:41:32 +0530

    test4

--- set01/problem04.c
+++ set01/problem04.c
@@ -17,7 +17,7 @@ void add(int a, int b, int *sum){
     *sum=a+b;
 }
 void output(int a, int b, int sum){
-    printf("Sum of %d and %d is %d",a, b, sum);
+    printf("Sum of %d and %d is %d\n",a, b, sum);
 }
 int main(){
     int a, b;
