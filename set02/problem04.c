23a17c82ce28d6493845d1af75fe2d9ae2bc8707
Fiza-A <fizaanwar.bsc23@rvu.edu.in>
2024-02-16 20:22:48 +0530

    test

--- set02/problem04.c
+++ set02/problem04.c
@@ -21,6 +21,40 @@ void input_array(int n, int a[n]){
         scanf("%d",&a[i]);
     }
 }
+int is_composite(int n, int a[n]){
+    for(int i=0;i<n;i++){
+        if(a[i]<=1){
+            return 0;
+        }
+        if(a[i]==2 || a[i]==3){
+            return -1;
+        }
+        for(int j = 2; j<=a[i]/2; j++){
+            if(a[i]%j==0){
+                return 1;
+            }
+        }
+    }
+}
 int sum_composite_numbers(int n, int a[n]){
-    
+    int sum=0;
+    int res;
+    for(int i=0;i<n;i++){
+        if(res==1){
+            sum+=a[i];
+        }
+    }
+    return sum;
+}
+void output(int sum){
+    printf("The sum of all composite numbers from the entered numbers is %d",sum);
+}
+int main(){
+    int n, sum;
+    n=input_array_size();
+    int a[n];
+    input_array(n,a);
+    sum=sum_composite_numbers(n,a);
+    output(sum);
+    return 0;
 }
