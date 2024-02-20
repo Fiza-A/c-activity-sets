2422938a2eef2742e9af373d27d28d448605efdb
Fiza-A <fizaanwar.bsc23@rvu.edu.in>
2024-02-20 19:54:10 +0530

    test

---
+++ set02/problem06.c
@@ -0,0 +1,16 @@
+/*06. Write a program to reverse a string.
+
+***Function Declarations***
+```c
+void input_string(char *a);
+void str_reverse(char *str, char *rev_str);
+void output(char *a, char *reverse_a);
+```*/
+#include<stdio.h>
+void input_string(char *a);
+void str_reverse(char *str, char *rev_str);
+void output(char *a, char *reverse_a);
+int main(){
+    char a, rev_str;
+    
+}
