03799ba24d4d6c7446d4f361aa8067db1763d9a3
Fiza-A <fizaanwar.bsc23@rvu.edu.in>
2024-02-26 12:42:35 +0530

    test

--- set02/problem06.c
+++ set02/problem06.c
@@ -21,6 +21,17 @@ void input_string(char *a){
     printf("Enter a string:\n");
     scanf("%s", a);
 }
-void str_reverse(char *str, char *rev_str){
+void str_reverse(char *str, char *rev_str) {
+    int length = strlen(str);
+    int i, j;
     
+    for (i = length - 1, j = 0; i >= 0; i--, j++) {
+        rev_str[j] = str[i];
+    }
+    rev_str[j] = '\0';
 }
+void output(char *a, char *reverse_a) {
+    printf("\nGiven String: %s\nReversed String: %s\n", a, reverse_a);
+}
+
+
